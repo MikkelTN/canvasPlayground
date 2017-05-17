86d0da9f9c9ae9089f59ef633b517f96c1369698
Mikkel Trøst Nielsen <mtn.dtu@gmail.com>
2017-05-17 13:47:56 +0200

    changed dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/docs',
     filename: 'app.bundle.js'
   },
   module: {
